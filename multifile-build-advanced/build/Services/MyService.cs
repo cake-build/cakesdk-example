96c4a04cfa2749b624d1ed9d773ec4ba8753e58d
Mattias Karsson <mattias@devlead.se>
2026-02-26 21:37:49 +0000

    chore(deps): update dependency gitversion.tool to 6.6.0

--- multifile-build-advanced/build/Services/MyService.cs
+++ multifile-build-advanced/build/Services/MyService.cs
@@ -2,7 +2,7 @@ public class MyService : IMyService
 {
     public string GetVersion()
     {
-        InstallTool("dotnet:https://api.nuget.org/v3/index.json?package=GitVersion.Tool&version=6.3.0");
+        InstallTool("dotnet:https://api.nuget.org/v3/index.json?package=GitVersion.Tool&version=6.6.0");
         var version = GitVersion();
         return version.FullSemVer;
     }
